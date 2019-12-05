66d780fddeeb75375120d9693af9bb1da27a73d2
KonH <konh@yandex.ru>
2019-12-05 20:52:39 +0700

    Simple LED blinking

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Abstractions;
+using Unosquare.WiringPi;
+
+namespace PiGpioSamples {
+	class Program {
+		static void Main(string[] args) {
+			Pi.Init<BootstrapWiringPi>();
+			var pin = Pi.Gpio[BcmPin.Gpio17];
+			pin.PinMode = GpioPinDriveMode.Output;
+			var isOn = false;
+			for ( var i = 0; i < 10; i++ ) {
+				isOn = !isOn;
+				pin.Write(isOn);
+				System.Threading.Thread.Sleep(500);
+			}
+		}
+	}
+}
